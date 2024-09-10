92c4fd979a2b7b6386d0daedafe42a03611c66b8
Satyam Mishra <112871870+satyam-mishra-07@users.noreply.github.com>
2024-09-10 23:24:06 +0530

    Done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Footer from "./components/Footer";
 import Menu from "./pages/Menu";
 import Dine from "./pages/Dine";
