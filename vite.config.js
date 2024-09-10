de6ea56c143f44727e29b94367588432a6bd9157
Satyam Mishra <112871870+satyam-mishra-07@users.noreply.github.com>
2024-09-11 00:15:45 +0530

    changed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   build: {
     outDir: 'dist', // Ensure output is directed to the correct directory
   }
